24a430173ceab342820273fda7dcfec685526e9a
Nanda WK <nandawinkyu.ix@gmail.com>
2025-03-29 17:25:11 +0700

    Initial Commit

---
+++ KoeNaWin/KoeNaWinApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoeNaWinApp.swift
+//  KoeNaWin
+//
+//  Created by Nanda WK on 2025-03-29.
+//
+
+import SwiftUI
+
+@main
+struct KoeNaWinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
