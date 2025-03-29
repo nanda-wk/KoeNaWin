8a14b47e375a7488105b8d445504b8517795d558
Nanda WK <nandawinkyu.ix@gmail.com>
2025-03-29 18:21:28 +0700

    feat: add home screen ui

---
+++ KoeNaWin/Features/Home/CustomProgressViewStyle.swift
@@ -0,0 +1,39 @@
+//
+//  CustomProgressViewStyle.swift
+//  KoeNaWin
+//
+//  Created by Nanda WK on 2025-03-29.
+//
+
+import SwiftUI
+
+
+struct CustomProgressViewStyle: ProgressViewStyle {
+    var height: CGFloat = 12
+    var progress: Color = .accent
+    var background: Color = .gray.opacity(0.2)
+    func makeBody(configuration: Configuration) -> some View {
+        VStack(spacing: 10) {
+            // Include the label from the original ProgressView
+            configuration.label
+
+            // Custom progress bar
+            GeometryReader { geometry in
+                ZStack(alignment: .leading) {
+                    // Background
+                    RoundedRectangle(cornerRadius: 8)
+                        .frame(height: height)
+                        .foregroundStyle(background)
+
+                    // Progress
+                    RoundedRectangle(cornerRadius: 8)
+                        .frame(width: CGFloat(configuration.fractionCompleted ?? 0) * geometry.size.width, height: height) // Adjust height here
+                        .foregroundStyle(progress)
+                }
+            }
+            .frame(height: height) // Match the height above
+
+            configuration.currentValueLabel
+        }
+    }
+}
--- KoeNaWin/Features/Home/HomeScreen.swift
+++ KoeNaWin/Features/Home/HomeScreen.swift
@@ -6,13 +6,139 @@
 //
 
 import SwiftUI
+import Charts
 
 struct HomeScreen: View {
     var body: some View {
-        Text("Home Screen")
+        List {
+            completionSection
+
+            currentStageCompletion
+
+            todayMantra
+
+            vegetarianSection
+        }
+        .listSectionSpacing(25)
+        .navigationTitle("ကိုးနဝင်း")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+extension HomeScreen {
+    var completionSection: some View {
+        Section {
+            VStack {
+                Text("အဓိဌာန်ပြီးမြောက်မှု")
+                    .font(.title2)
+                    .fontWeight(.bold)
+
+                HStack {
+                    Chart {
+                        SectorMark(
+                            angle: .value("Done", 10),
+                            innerRadius: .ratio(0.65),
+                            angularInset: 2
+                        )
+                        .cornerRadius(10)
+
+                        SectorMark(
+                            angle: .value("In Progress", 81 - 10),
+                            innerRadius: .ratio(0.65),
+                            angularInset: 2
+                        )
+                        .foregroundStyle(.gray.opacity(0.2))
+                        .cornerRadius(10)
+                    }
+                    .chartBackground{ proxy in
+                        Text("10 / 81")
+                    }
+                    .padding()
+
+                    VStack(spacing: 20) {
+                        Text("10%")
+                            .font(.headline)
+                            .foregroundStyle(.white)
+                            .padding(.horizontal)
+                            .padding(.vertical, 4)
+                            .frame(maxWidth: .infinity)
+                            .background(
+                                Capsule(style: .circular)
+                                    .fill(.accent)
+                            )
+                        HStack {
+                            Image(systemName: "staroflife.fill")
+                                .font(.caption2)
+
+                            Text("71 ရက်ကျန်သည်")
+                                .lineLimit(1, reservesSpace: true)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    var currentStageCompletion: some View {
+        Section {
+            VStack {
+                Text("အဓိဌာန်အဆင့် (2)")
+                    .font(.title2)
+                    .fontWeight(.bold)
+
+                ProgressView(value: 1, total: 9) {
+                } currentValueLabel: {
+                    HStack {
+                        Text("10%")
+                        Spacer()
+                        Text("1 / 9 ရက်")
+                    }
+                    .font(.subheadline)
+                }
+                .progressViewStyle(CustomProgressViewStyle(height: 20))
+            }
+        }
+    }
+
+    var todayMantra: some View {
+        Section {
+            VStack(spacing: 10) {
+                HStack {
+                    Text("တနင်္လာ")
+                    Spacer()
+                    Text("အဓိဌာန်အဆင့် (2)")
+                }
+                .font(.subheadline)
+                .fontWeight(.medium)
+                .foregroundStyle(.accent)
+
+
+                Text("ဘဂဝါ")
+                    .font(.title)
+                    .fontWeight(.bold)
+
+                Text("အပတ်ရေ (9)ပတ်")
+                    .font(.body)
+                    .foregroundStyle(.secondary)
+
+            }
+        }
+    }
+
+    var vegetarianSection: some View {
+        Section {
+            Text("3 day untail vegetarian.")
+                .foregroundStyle(.white)
+        }
+        .listRowBackground(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.accent)
+        )
     }
 }
 
 #Preview {
-    HomeScreen()
+    NavigationStack {
+        HomeScreen()
+    }
 }
